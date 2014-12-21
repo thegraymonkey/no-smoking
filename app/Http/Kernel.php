4137d067f8b692ad5d95111eae1564e0f2f4a5dc
Gil <dragan@6min.de>
2014-12-21 14:17:36 +0100

    added admin auth middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -26,6 +26,7 @@ class Kernel extends HttpKernel {
 	protected $routeMiddleware = [
 		'auth' => 'App\Http\Middleware\Authenticate',
 		'auth.basic' => 'Illuminate\Auth\Middleware\AuthenticateWithBasicAuth',
+		'auth.admin' => 'App\Http\Middleware\AdminAuth',
 		'guest' => 'App\Http\Middleware\RedirectIfAuthenticated',
 	];
 
