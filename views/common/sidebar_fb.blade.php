55693538bdd0d7518ac3c89a693bb4e0d27445dd
thegraymonkey <thegraymonkey@gmail.com>
2015-01-29 18:08:07 +0100

    20px

--- views/common/sidebar_fb.blade.php
+++ views/common/sidebar_fb.blade.php
@@ -5,7 +5,7 @@
 	<div class="fb-share-button" data-href="http://zabranjenopusenje.net/" data-layout="button"></div> 
 	<div style="margin-top:6px;"
   		class="fb-like" 
-  		data-width="140"
+  		data-width="160"
   		data-show-faces="true">
 	</div>
 	 
