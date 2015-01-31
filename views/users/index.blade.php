2f71dff9902c867dd420a1ef1a2d975061fd51bd
thegraymonkey <thegraymonkey@gmail.com>
2015-01-31 21:55:25 +0100

    list of users

--- views/users/index.blade.php
+++ views/users/index.blade.php
@@ -30,7 +30,7 @@
 		
 		<td>{{ $user->id }}</td>		
 		<td>{{ $user->username }}</td>
-		@if($user->profile->avatar)
+		@if($user->profile)
 		<td><img src="/upload/profile/{{ $user->profile->getStatusAvatar() }}"/></td>
 		@else
 		<td><img src="/images/blank.png"/></td>
