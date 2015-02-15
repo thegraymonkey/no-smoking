33ea31d03a0065cbaf871d765ba194609018f44e
thegraymonkey <thegraymonkey@gmail.com>
2015-02-15 11:28:22 +0100

    google analytics code

--- views/layout.blade.php
+++ views/layout.blade.php
@@ -46,7 +46,8 @@
   </head>
 <!-- NAVBAR
 ================================================== -->
-  
+ <body>
+
   <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">    
 		<div class="container" >
 			<div class="navbar-header">
@@ -121,7 +122,15 @@
 					  
     	</div>
     </footer>
-	
+	<script>
+	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+	  ga('create', 'UA-59779521-1', 'auto');
+	  ga('send', 'pageview');
+	</script>
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
     <script src="/bootstrap/js/bootstrap.min.js"></script>
     <script src="/bootstrap/js/docs.min.js"></script>
